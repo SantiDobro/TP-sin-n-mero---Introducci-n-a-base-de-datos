f168d1467dc3ca8e521cfe891afa8f2fffbd405a
SANTIAGO IGAL DOBROVITZKY <49317356@ort>
2025-07-04 08:30:04 -0300

    first commit

---
+++ TP sin número - Introducción a base de datos/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP_sin_número___Introducción_a_base_de_datos.Models;
+
+namespace TP_sin_número___Introducción_a_base_de_datos.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ TP sin número - Introducción a base de datos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_sin_número___Introducción_a_base_de_datos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
